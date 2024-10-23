4a595ef0fb5f1761a3e7f26f1dd6b9a612ecbf7d
mmrmunna786 <mmrmunna786@gmail.com>
2024-10-23 01:36:16 +0100

    commit 2

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10; // x is the referent.
+    int &ref = x; // ref is the  reference to x. We must always initialise a reference with a variable to which it is
+    // referring to.
+    // a reference always need an initializer.
+    return 0;
+}
